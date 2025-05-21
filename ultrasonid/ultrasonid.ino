f98daddb5022f7da8a8c829fb0d9e395ec3adc19
Julio Mantilla <=>
2025-05-21 10:42:05 -0500

    first commit

---
+++ ultrasonid/ultrasonid.ino
@@ -0,0 +1,109 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <Arduino_JSON.h>
+#include <SPI.h>
+// 2025
+#define HOST "localhost" //Esto se deja quieto
+#define WIFI_SSID "Flia Mantilla" //El nombre de tu red
+#define WIFI_PASSWORD "5645485MR" // La contraseña de tu red
+//define sound speed in cm/uS
+#define SOUND_SPEED 0.034
+#define CM_TO_INCH 0.393701
+
+const int trigPin = 5;
+const int echoPin = 18;
+
+long duration;
+float distanceCm;
+float distanceInch;
+String sendval, sendval2, postData;
+
+void checkWiFiConnection() 
+{
+
+  while (WiFi.status() != WL_CONNECTED)
+   {
+    Serial.println("Connecting to Wi-Fi...");
+    delay(1000);
+  }
+
+  Serial.println("Connected to Wi-Fi");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+}
+
+void setup() 
+{
+  Serial.begin(115200); // Starts the serial communication
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  Serial.println("Communication Started\n");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  checkWiFiConnection();
+}
+
+void loop()
+ {
+  checkWiFiConnection();
+  WiFiClient client;
+  HTTPClient http;
+
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  
+  // Calculate the distance
+  distanceCm = duration * SOUND_SPEED/2;
+  
+  // Convert to inches
+  distanceInch = distanceCm * CM_TO_INCH;
+  
+  // Prints the distance in the Serial Monitor
+  Serial.print("Distance (cm): ");
+  Serial.println(distanceCm); 
+
+  postData = "sendval=" + String(distanceCm) + "&sendval2=" + "1";
+  Serial.print("Sending data: ");
+  Serial.println(postData);
+
+  http.begin(client, "http://192.168.1.6/ultrasonid/test.php"); //Aqui pones tu ip (Ejecutas el ipconfig en la consola de windows) y la dirección de tu main de php
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  int httpCode = http.POST(postData);
+
+  if (httpCode == HTTP_CODE_OK) 
+  {
+    String response = http.getString();
+    Serial.println("Server response: " + response);
+  } 
+  
+  else
+  {
+    Serial.print("HTTP POST request failed with error code: ");
+    Serial.println(httpCode);
+    if (httpCode == HTTPC_ERROR_CONNECTION_REFUSED)
+    {
+      Serial.println("Connection refused by the server.");
+    }
+    else if (httpCode == HTTP_CODE_NOT_FOUND) 
+    {
+      Serial.println("Server resource not found.");
+    }
+    else {
+      Serial.println("Unknown error occurred.");
+    }
+  }
+
+  http.end();
+  delay(5000);
+}
