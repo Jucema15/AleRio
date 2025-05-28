066a74645122edfa50eec30bb5de8e52f1d8fa8a
Julio Mantilla <=>
2025-05-28 16:05:50 -0500

    Se arregla cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(3000);
+
+  app.enableCors({
+    origin: [
+      'https://angular-map-rho.vercel.app', // Reemplaza con tu dominio real de Vercel
+    ],
+    credentials: true, // Si usas cookies o autenticación
+  });
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
