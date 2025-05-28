31e2dca70245c0ecb945a6e5a39b374350568ea6
Julio Mantilla <=>
2025-05-28 17:26:10 -0500

    Cambio main

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors({
-    origin: [
-      'https://angular-map-rho.vercel.app', // Reemplaza con tu dominio real de Vercel
-    ],
-    credentials: true, // Si usas cookies o autenticación
-  });
-
-  await app.listen(process.env.PORT || 3000);
+  app.enableCors();
+  await app.listen(3000);
 }
 bootstrap();
